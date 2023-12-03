7e619b95a718b0ef1b3911ef7cc98ba7dc56bdf5
Darren Korman <darrenkorman@hotmail.com>
2023-12-03 12:58:14 -0800

    Comparison works

--- day2/day2.ps1
+++ day2/day2.ps1
@@ -1,45 +1,62 @@
 # https://adventofcode.com/2023/day/2
 
-$Content = Get-Content "input-sample.txt"
+$Content = Get-Content "input.txt"
 
 $SumOfGameIDs = 0
 
 $MaxResult = @{"red"=12; "green"=13; "blue"=14}
 
 $Content | ForEach-Object -Process {
-    Write-Host $_
+    Write-Host "Input=$_"
 
     $LineSplit = $_.Split(":")
 
     $GameID = $LineSplit[0].Substring("Game ".Length)
-    Write-Host "GameID: $GameID"
     $GamePossible = $true
 
     $Games = $LineSplit[1].Split(";")
     $Games | ForEach-Object -Process {
-        $GrabResult = @{}
+        $GrabPossible = $true
+        $GrabResult = @{"red"=0; "green"=0; "blue"=0}
 
         $DiceGrabs = $_.Split(",")
         $DiceGrabs | ForEach-Object -Process {
             $GrabSplit = $_.Trim().Split(" ")
             $Count = $GrabSplit[0]
             $Color = $GrabSplit[1]
-            # Write-Host "Dice: $GrabSplit, $Color $Count"
             $GrabResult[$Color] = $Count
         }
 
         Write-Host $GrabResult
-        if ($GrabResult["red"] -le $MaxResult["red"] -and $GrabResult["green"] -le $MaxResult["green"] -and $GrabResult["blue"] -le $MaxResult["blue"]) {
-            Write-Host "Possible game"
+        
+        if ([int]$GrabResult["red"] -gt $MaxResult["red"]) {
+            $GrabPossible = $false
+            Write-Host "Grab red > Max red"
+        }
+
+        if ([int]$GrabResult["green"] -gt $MaxResult["green"]) {
+            $GrabPossible = $false
+            Write-Host "Grab green > Max green"
+        }
+
+        if ([int]$GrabResult["blue"] -gt $MaxResult["blue"]) {
+            $GrabPossible = $false
+            Write-Host "Grab blue > Max blue"
+        }
+
+        if ($GrabPossible) {
+            Write-Host "Possible Grab"
         } else {
-            Write-Host "Impossible Game"
+            Write-Host "Impossible Grab"
             $GamePossible = $false
         }
     }
 
-    if ($GamePossible -eq $false) {
+    Write-Host "GameID: $GameID, Possible=$GamePossible"
+
+    if ($GamePossible) {
         $SumOfGameIDs += $GameID
     }
 }
 
-Write-Host "Sum of GameIDs=$SumOfGameIDs"
+Write-Host "Sum of Possible GameIDs=$SumOfGameIDs"
